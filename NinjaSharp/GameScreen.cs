949a93bdff2ddcf3f47ea8fc16a8d7f3918f37ab
Jesse Chounard <jesse@thirdpartyninjas.com>
2013-09-30 14:21:24 -0500

    Added ScreenManager and GameScreen

---
+++ NinjaSharp/GameScreen.cs
@@ -0,0 +1,96 @@
+﻿namespace ThirdPartyNinjas.NinjaSharp
+{
+	public abstract class GameScreen
+	{
+		public enum State
+		{
+			Waiting,
+			TransitionOn,
+			Active,
+			TransitionOff,
+		}
+
+		public bool IsExclusive { get; protected set; }
+		public bool HasFocus { get; private set; }
+
+		public State ScreenState { get; set; }
+		public ScreenManager ScreenManager { get; set; }
+
+		public bool Remove { get; protected set; }
+
+		public bool TransitionOn { get; protected set; }
+		public float TransitionOnTime { get; protected set; }
+		public bool TransitionOff { get; protected set; }
+		public float TransitionOffTime { get; protected set; }
+		public float TransitionPosition { get; protected set; }
+		
+		public GameScreen()
+		{
+			TransitionOn = TransitionOff = false;
+			TransitionOnTime = TransitionOffTime = 0.0f;
+			TransitionPosition = 0;
+
+			IsExclusive = true;
+			Remove = false;
+		}
+
+		public void Exit(bool allowTransition)
+		{
+			if (!allowTransition || TransitionOff == false)
+			{
+				Remove = true;
+			}
+			else
+			{
+				ScreenState = State.TransitionOff;
+			}
+		}
+
+		public void Update(float deltaSeconds, bool hasFocus)
+		{
+			HasFocus = hasFocus;
+
+			switch (ScreenState)
+			{
+				case State.Waiting:
+					if (TransitionOn)
+					{
+						TransitionPosition = 0;
+						ScreenState = State.TransitionOn;
+					}
+					else
+					{
+						TransitionPosition = 1;
+						ScreenState = State.Active;
+					}
+					return;
+
+				case State.TransitionOn:
+					TransitionPosition += deltaSeconds / TransitionOnTime;
+					if (TransitionPosition >= 1)
+					{
+						TransitionPosition = 1;
+						ScreenState = State.Active;
+					}
+					break;
+
+				case State.TransitionOff:
+					TransitionPosition -= deltaSeconds / TransitionOffTime;
+					if (TransitionPosition <= 0)
+					{
+						TransitionPosition = 0;
+						Remove = true;
+					}
+					break;
+
+				default:
+					break;
+			}
+		
+			Update(deltaSeconds);
+		}
+
+		public abstract void Update(float deltaSeconds);
+		public abstract void Draw();
+	}
+}
---
+++ NinjaSharp/ScreenManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace ThirdPartyNinjas.NinjaSharp
+{
+	public class ScreenManager
+	{
+		public void Update(float deltaSeconds)
+		{
+			screens.AddRange(screensToAdd);
+			screensToAdd.Clear();
+
+			GameScreen waitingScreen = null;
+			bool hasFocus = true;
+			bool foundExclusive = false;
+
+			for (int i = screens.Count - 1; i >= 0; i--)
+			{
+				GameScreen gameScreen = screens[i];
+
+				if (gameScreen.ScreenState == GameScreen.State.Waiting)
+				{
+					if (waitingScreen == null)
+						waitingScreen = gameScreen;
+					else
+						gameScreen.Exit(false);
+				}
+				else
+				{
+					if (gameScreen.IsExclusive)
+					{
+						if (foundExclusive)
+							gameScreen.Exit(true);
+						else
+							foundExclusive = true;
+					}
+
+					gameScreen.Update(deltaSeconds, hasFocus);
+					hasFocus = false;
+				}
+			}
+
+			foundExclusive = false;
+			waitingScreen = null;
+
+			int index = 0;
+			while (index < screens.Count)
+			{
+				if (screens[index].Remove)
+				{
+					screens.RemoveAt(index);
+				}
+				else
+				{
+					if (screens[index].ScreenState == GameScreen.State.Waiting)
+						waitingScreen = screens[index];
+					else
+						foundExclusive = true;
+					index++;
+				}
+			}
+
+			if (waitingScreen != null && !foundExclusive)
+			{
+				waitingScreen.Update(0, true);
+			}
+		}
+
+		public void Draw()
+		{
+			for (int i = screens.Count - 1; i >= 0; i--)
+			{
+				if (screens[i].ScreenState != GameScreen.State.Waiting)
+					screens[i].Draw();
+			}
+		}
+
+		public void AddScreen(GameScreen gameScreen)
+		{
+			gameScreen.ScreenManager = this;
+
+			if (!gameScreen.IsExclusive)
+				gameScreen.ScreenState = GameScreen.State.TransitionOn;
+			else
+				gameScreen.ScreenState = GameScreen.State.Waiting;
+
+			screensToAdd.Add(gameScreen);
+		}
+
+		List<GameScreen> screens = new List<GameScreen>();
+		List<GameScreen> screensToAdd = new List<GameScreen>();
+	}
+}
